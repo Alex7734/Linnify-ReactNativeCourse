f8bd3638ab508a24203b1c2eecbb70283dbd6880
Bogdan <bogdan.dobai@linnify.com>
2023-04-24 09:25:47 +0300

    fix(books): Fix crash on useSearch

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,9 +5,12 @@ import { useSearch } from "../hooks/use-search.hook";
 import { data } from "../data/BookListData";
 
 export const SearchBar = (search: string) => {
-    const [state, setState] = useState<string>(search)
+    // TODO: Main issue (the crash) -> You are not sending this search param. So the default value is {}.
+    //  Therefore you'll send the {} to the useSearch. And you are trying to call .toLowerCase on an empty object.
+    const [state, setState] = useState<string>(typeof search === 'string' ? search : '')
     const debounce = useDebounce(state, 500);
-    const searchResults = useSearch(data, debounce, 'name');;
+    const searchResults = useSearch(data, debounce, 'name');
+    // TODO: You will have to move the useSearch hook in the book list component so you can actually filter the given data
 
     useEffect(() => {
         console.log('debounce', debounce)
@@ -25,11 +28,11 @@ export const SearchBar = (search: string) => {
 
 const styles = StyleSheet.create({
     searchBar: {
-        height: 40, 
-        width: '100%', 
+        height: 40,
+        width: '100%',
         paddingHorizontal: 3,
-        marginVertical: 10, 
-        borderWidth: 2 , 
+        marginVertical: 10,
+        borderWidth: 2 ,
         backgroundColor: '#fffffa'
     }
 })
